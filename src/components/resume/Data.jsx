68e08329e528c74aa28ebb045002a58747d7f7b9
Edun Joshua <joshuaolubori@gmail.com>
2023-02-24 04:51:04 +0100

    initial commit

---
+++ src/components/resume/Data.jsx
@@ -0,0 +1,46 @@
+export const cv = [
+    {
+        id: 1,
+        title: 'Federal University of Agriculture, Abeokuta',
+        subtitle: 'Department of Agricultural and Bioresources Engineering',
+        date: '2017 - 2022',
+        description: 'First Class Grad (4.55/5.00); Departmental Association Electoral Chairman (2019 - 2021); Chevron Scholar (2017)',
+        category: 'education'
+    },
+
+    {
+        id: 2,
+        title: 'ALX - Udacity',
+        subtitle: 'Data Analytics Nanodegree',
+        date: '2022',
+        description: 'First to bag Nanodegree in cohort',
+        category: 'education'
+    },
+
+    {
+        id: 3,
+        title: 'Google',
+        subtitle: 'Google Data Analytics',
+        date: '2022',
+        description: 'Google Certificates - Coursera',
+        category: 'education'
+    },
+
+    {
+        id: 4,
+        title: 'PwC Switzerland',
+        subtitle: 'Virtual Internship',
+        date: '2022',
+        description: 'Created dashboards with PowerBI to reflect relevant KPIs',
+        category: 'experience'
+    },
+
+    {
+        id: 5,
+        title: 'KPMG Australia',
+        subtitle: 'Virtual Internship',
+        date: '2022',
+        description: 'Optimised marketing strategy for biking company with Tableau',
+        category: 'experience'
+    },
+]
---
+++ src/components/work/Data.jsx
@@ -0,0 +1,65 @@
+import Work1 from '../../assets/portfolio_images/work1.jpg';
+import Work2 from '../../assets/portfolio_images/work2.jpg';
+import Work3 from '../../assets/portfolio_images/work3.jpg';
+import Work4 from '../../assets/portfolio_images/work4.png';
+import Work5 from '../../assets/portfolio_images/work5.png';
+
+export const projectsData = [
+    {
+        id: 1,
+        image: Work1,
+        title: "COVID-19 Prevalence in Nigeria",
+        category: "Viz",
+        code_link: '#1',
+        demo_link: 'https://public.tableau.com/app/profile/joshua1294/viz/Covid-19InNigeria/Dashboard'
+
+    },
+    {
+        id: 2,
+        image: Work2,
+        title: "Year-by-year Analysis on Gun Sales in the U.S",
+        category: "Others",
+        code_link: 'https://github.com/JoshuaOlubori/Data-Science-Analytics-Portfolio-Projects/blob/main/NCIS_FBI_Gun_Analysis/',
+        demo_link: 'https://github.com/JoshuaOlubori/Data-Science-Analytics-Portfolio-Projects/blob/main/NCIS_FBI_Gun_Analysis/fbi_gun_data.ipynb'
+    },
+    {
+        id: 3,
+        image: Work3,
+        title: "Designing a PowerBI Dashboard for Customer Retention",
+        category: "Viz",
+        code_link: 'https://github.com/JoshuaOlubori/Data-Science-Analytics-Portfolio-Projects/tree/main/PwC%20Customer%20Retention/',
+        demo_link: 'https://drive.google.com/file/d/1ChEzpHpmefpUMc1fAm5EmdykTZVF0opE/view?usp=sharing'
+    },
+    {
+        id: 4,
+        image: Work4,
+        title: "Call Center Insights Using PowerBI (PwC Internship)",
+        category: "Viz",
+        code_link: 'https://github.com/JoshuaOlubori/Data-Science-Analytics-Portfolio-Projects/tree/main/PwC%20Customer%20Retention/',
+        demo_link: 'https://drive.google.com/file/d/1fhWkYH1cs2Ca4C5S7KoZQ9q5p2ChFzfY/view?usp=share_link'
+    },
+    {
+        id: 5,
+        image: Work5,
+        title: "Tweet Analysis",
+        category: "Others",
+        code_link: 'https://github.com/JoshuaOlubori/Data-Science-Analytics-Portfolio-Projects/tree/main/Udacity_Project_2',
+        demo_link: 'https://www.kaggle.com/code/joshuaolubori/tweet-data-wrangling-and-analysis'
+    },
+];
+
+export const projectsNav = [
+    {
+        name: 'all',
+    },
+    {
+        name: 'SQL',
+    },
+    {
+        name: 'Viz',
+    },
+    {
+        name: 'Others'
+    },
+];
+
